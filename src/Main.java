ba01c8fe2a76f7c69f8d589601c18c43029d8ee5
Azat <aelchikushuulu@gmail.com>
2023-02-27 21:03:22 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    private static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    private static void task1() {
+        System.out.println("Задача 1");
+
+        int[] arr = generateRandomArray();
+        System.out.println("«Сгенерированный массив: " + Arrays.toString(arr) + " рублей».");
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("«Сумма трат за месяц составила " + sum + " рублей».");
+    }
+}
